7c2e3e6a4ce138156a03e6fe75f1959c2ec6ffa3
Harpal Jadeja <jadejaharpal14@gmail.com>
2024-02-15 19:15:49 +0530

    chore: fix url for development env

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 export const BASE_URL =
     process.env.NODE_ENV === "production"
         ? "https://find-farcaster-friends.vercel.app"
-        : process.env.NEXT_PUBLIC_VERCEL_URL;
+        : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 
 export const ERROR_IMAGE_URL = `${BASE_URL}/error.png`;
 export const WALLET_NOT_CONNECTED_IMAGE_URL = `${BASE_URL}/wallet_not_connected.png`;
